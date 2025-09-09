21489f0699f9fee45b807eab78968a1fc90de1a7
Jose Landim <joselandimsobrinho@hotmail.com>
2025-09-09 13:10:11 -0300

    Implementação da classe Processos.

--- src/Processos.java
+++ src/Processos.java
@@ -1,2 +1,56 @@
 public class Processos {
+    private int id;
+    private String nome;
+    private int prioridade; // 1-Alta, 2-Média e 3-Baixa
+    private int ciclosNecessarios;
+    private String recursoNecessario;
+
+    //construtor
+    public Processos (int id, String nome, int prioridade, int ciclosNecessarios, String recursoNecessario){
+        this.id = id;
+        this.nome = nome;
+        this.prioridade = prioridade;
+        this.ciclosNecessarios = ciclosNecessarios;
+        this.recursoNecessario = recursoNecessario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCiclosNecessarios() {
+        return ciclosNecessarios;
+    }
+
+    public void setCiclosNecessarios(int ciclosNecessarios) {
+        this.ciclosNecessarios = ciclosNecessarios;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public String getRecursoNecessario() {
+        return recursoNecessario;
+    }
+
+    public void setRecursoNecessario(String recursoNecessario) {
+        this.recursoNecessario = recursoNecessario;
+    }
 }
