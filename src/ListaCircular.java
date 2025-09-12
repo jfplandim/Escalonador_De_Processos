5791c9fbc48e4e70bbfff0b130d3f33dd281588e
ricgasparrr <ricgaspar13@gmail.com>
2025-09-12 16:38:48 -0300

    Implementação da classe lista circular

---
+++ src/ListaCircular.java
@@ -0,0 +1,46 @@
+public class ListaCircular {
+    private Node atual;
+    private int tamanho;
+
+    public void adicionar(Processos processo){
+        Node novo = new Node(processo);
+        if(atual == null){
+            atual = novo;
+            atual.next = atual;
+            atual.anterior = atual;
+        }
+        else{
+            Node ultimo = atual.anterior;
+            ultimo.next = novo;
+            novo.anterior = ultimo;
+            novo.next = atual;
+            atual.anterior = novo;
+        }
+        tamanho++;
+    }
+    public Processos getAtual(){
+        return atual != null ? atual.processo : null;
+    }
+    public void avancar(){
+        if(atual != null) atual = atual.next;
+    }
+    public Processos removerAtual(){
+        if(atual == null) return null;
+        Processos processos = atual.processo;
+        if(atual.next==atual){
+            atual=null;
+        }
+        else{
+            Node prox = atual.next;
+            atual.anterior.next = prox;
+            prox.anterior = atual.anterior;
+            atual = prox;
+        }
+        tamanho--;
+        return processos;
+    }
+    public int getTamanho(){
+        return tamanho;
+    }
+
+}
--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,2 +1,12 @@
 public class Scheduler {
+    private ListaDeProcessos lista_alta_prioridade;
+    private ListaDeProcessos lista_media_prioridade;
+    private ListaDeProcessos lista_baixa_prioridade;
+    private ListaDeProcessos lista_bloqueados;
+
+    private int contador_ciclos_alta_prioridade;
+
+    public Scheduler(){
+        this.lista_alta_prioridade = new ListaDeProcessos();
+        this.lista_media_prioridade
 }
