3d76a28de6a1664af6fc554bde007e357d3dabe3
João <joaoguimalta2712@gmail.com>
2025-09-12 16:09:12 -0300

    class ProcessosBloqueados

--- src/Node.java
+++ src/Node.java
@@ -1,8 +1,10 @@
 public class Node {
     Processos processo;
     Node next;
+    Node anterior;
     public Node(Processos processo){
         this.processo = processo;
         this.next = null;
+        this.anterior = null;
     }
 }
