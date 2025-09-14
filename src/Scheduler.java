39a9b1497a4102ce10d7924d3c77884e307fc600
ricardo.ruben <ricgaspar13@gmail.com>
2025-09-13 22:22:19 -0300

    ajeitado lista de inanição

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -10,18 +10,35 @@ public class Scheduler {
         this.lista_alta_prioridade = new ListaDeProcessos();
         this.lista_media_prioridade = new ListaDeProcessos();
         this.lista_baixa_prioridade = new ListaDeProcessos();
+        this.lista_bloqueados = new ListaDeProcessos();
         this.contador_ciclos_alta_prioridade = 0;
     }
 
     public void executarCicloCPU() {
+        Processos processoParaExecutar = null;
         if (contador_ciclos_alta_prioridade >= 5) {
-            Processos processoParaExecutar = null;
             if (lista_media_prioridade.tamanho > 0){
                 processoParaExecutar = lista_media_prioridade.removerDoInicio();
+                contador_ciclos_alta_prioridade = 0;
             }
-            if(processoParaExecutar != null) {
+            else if (lista_baixa_prioridade.tamanho > 0) {
+                processoParaExecutar = lista_baixa_prioridade.removerDoInicio();
                 contador_ciclos_alta_prioridade = 0;
             }
         }
+        else if (lista_alta_prioridade.tamanho > 0) {
+    processoParaExecutar = lista_alta_prioridade.removerDoInicio();
+    contador_ciclos_alta_prioridade++;
+        }
+        else if(lista_media_prioridade.tamanho > 0){
+            processoParaExecutar = lista_media_prioridade.removerDoInicio();
+            contador_ciclos_alta_prioridade = 0;
+            }
+        else if(lista_baixa_prioridade.tamanho > 0){
+            processoParaExecutar = lista_baixa_prioridade.removerDoInicio();
+            contador_ciclos_alta_prioridade = 0;
+        }
     }
+
 }
+
