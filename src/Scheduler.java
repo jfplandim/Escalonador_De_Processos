139ac3c8d919a8a28f42984d59a3778a7aecb6ef
ricardo.ruben <ricgaspar13@gmail.com>
2025-09-13 21:50:45 -0300

    ajeitei o "adicionarFim" e o "adicionarInicio" em lista circular.
    adicionado logica anti-inanição em scheduler.

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -6,7 +6,22 @@ public class Scheduler {
 
     private int contador_ciclos_alta_prioridade;
 
-    public Scheduler(){
+    public Scheduler() {
         this.lista_alta_prioridade = new ListaDeProcessos();
-        this.lista_media_prioridade = new
+        this.lista_media_prioridade = new ListaDeProcessos();
+        this.lista_baixa_prioridade = new ListaDeProcessos();
+        this.contador_ciclos_alta_prioridade = 0;
+    }
+
+    public void executarCicloCPU() {
+        if (contador_ciclos_alta_prioridade >= 5) {
+            Processos processoParaExecutar = null;
+            if (lista_media_prioridade.tamanho > 0){
+                processoParaExecutar = lista_media_prioridade.removerDoInicio();
+            }
+            if(processoParaExecutar != null) {
+                contador_ciclos_alta_prioridade = 0;
+            }
+        }
+    }
 }
